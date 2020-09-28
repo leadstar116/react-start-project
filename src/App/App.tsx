918efa67bdb39e1d96a267bed9757386863880a6
leadstar116 <supremedev116@gmail.com>
2020-09-28 16:32:20 +1000

    initial commit

---
+++ src/App/App.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { Switch, Route, Router } from 'react-router-dom'
+import HomePage from '../HomePage/HomePage'
+import { alertClear } from '../_actions/alert.actions'
+import { history } from '../_helpers/history'
+
+function App() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    history.listen(() => {
+        dispatch(alertClear());
+    });
+  }, [dispatch]);
+
+  return (
+    <div className="jumbotron min-vh-100 mb-0">
+      <div className="container">
+        <div className="bg-white">
+          <Router history={history}>
+            <Switch>
+              <Route exact path='/' component={ HomePage } />
+            </Switch>
+          </Router>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
